28b6d050942db7fd4be38bcf5dbe52add64f6d98
Eronarn Palazzo <eronarn@gmail.com>
2013-01-03 10:10:51 -0500

    Lava orcs have slow movement when cold and fast movement when hot.

--- crawl-ref/source/mon-stuff.cc
+++ crawl-ref/source/mon-stuff.cc
@@ -4813,6 +4813,8 @@ bool temperature_effect(int which) {
 
     switch (which)
     {
+        case LORC_SLOW_MOVE:
+            return (temperature() < TEMP_COOL); // 1-4
         case LORC_STONESKIN:
             return (temperature() < TEMP_WARM); // 1-8
 //      case nothing, right now:
@@ -4826,6 +4828,7 @@ bool temperature_effect(int which) {
         case LORC_COLD_VULN:
         case LORC_PASSIVE_HEAT:
             return (temperature() >= TEMP_HOT); // 11-15
+        case LORC_FAST_MOVE:
         case LORC_NO_SCROLLS:
         case LORC_HEAT_AURA:
             return (temperature() >= TEMP_FIRE); // 13-15
